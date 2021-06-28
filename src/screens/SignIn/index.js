b49ef82df9b26ce9c061c62aa07738812aa3ad47
viniciusvasconcelosferreira <viniciusvasconcelos.ferreira@gmail.com>
2021-06-28 20:59:56 -0300

    Tela de login

---
+++ src/screens/SignIn/index.js
@@ -0,0 +1,47 @@
+import React, {useState} from 'react';
+import {
+  Container,
+  InputArea,
+  CustomButton,
+  CustomButtonText,
+  SignMessageButton,
+  SignMessageButtonText,
+  SignMessageButtonTextBold,
+} from './styles';
+import SignInput from '../../components/SignInput';
+import BarberLogo from '../../assets/barber.svg';
+import EmailIcon from '../../assets/email.svg';
+import LockIcon from '../../assets/lock.svg';
+export default () => {
+  const [emailField, setEmailField] = useState('');
+  const [passwordField, setPasswordField] = useState('');
+  return (
+    <Container>
+      <BarberLogo width="100%" height="160" />
+      <InputArea>
+        <SignInput
+          IconSvg={EmailIcon}
+          placeholder="Digite seu e-mail"
+          value={emailField}
+          onChangeText={t => setEmailField(t)}
+        />
+        <SignInput
+          IconSvg={LockIcon}
+          placeholder="Digite sua senha"
+          value={passwordField}
+          onChangeText={t => setPasswordField(t)}
+          password={true}
+        />
+        <CustomButton>
+          <CustomButtonText>LOGIN</CustomButtonText>
+        </CustomButton>
+      </InputArea>
+      <SignMessageButton>
+        <SignMessageButtonText>
+          Ainda não possui uma conta?
+        </SignMessageButtonText>
+        <SignMessageButtonTextBold>Cadastre-se</SignMessageButtonTextBold>
+      </SignMessageButton>
+    </Container>
+  );
+};
